76cf31b74531a34f9b9abbb2a9105745561ed94a
hikiasi <ivangf@list.ru>
2024-06-20 02:57:08 +0200

    auth form, button ui

--- client/app/components/screens/auth/Auth.tsx
+++ client/app/components/screens/auth/Auth.tsx
@@ -1,12 +1,54 @@
 import { FC, useState } from 'react'
-import { Text, View } from 'react-native'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Pressable, Text, View } from 'react-native'
+
+import Loader from '@/components/ui/Loader'
+import Button from '@/components/ui/button/Button'
+
+import { IAuthFormData } from '@/types/auth.interface'
+import AuthFields from './AuthFields'
 
 const Auth: FC = () => {
 	const [isReg, setIsReg] = useState(false)
 
+	const { handleSubmit, reset, control } = useForm<IAuthFormData>({
+		mode: 'onChange'
+	})
+
+	const onSubmit: SubmitHandler<IAuthFormData> = data => {
+		console.log(data)
+	}
+
+	const isLoading = false
+
 	return (
-		<View>
-			<Text>Auth</Text>
+		<View className='mx-2 items-center justify-center h-full'>
+			<View className='w-9/12'>
+				<Text className='text-center text-black text-3xl font-medium mb-8'>
+					{isReg ? 'Зарегистрироваться' : 'Войти'}
+				</Text>
+				{isLoading ? (
+					<Loader />
+				) : (
+					<>
+						<AuthFields control={control}/>
+
+
+						<Button onPress={handleSubmit(onSubmit)}>
+							{isReg ? 'Зарегистрироваться ' : 'Войти'}
+						</Button>
+
+						<Pressable onPress={() => setIsReg(!isReg)}>
+							<Text className='text-black text-center text-base mt-6'>
+								{isReg ? 'Уже есть аккаунт?' : 'Нет аккаунта?'}{' '}
+								<Text className='text-[#8968ff]'>
+									{isReg ? 'Войти' : 'Зарегистрироваться'}
+								</Text>
+							</Text>
+						</Pressable>
+					</>
+				)}
+			</View>
 		</View>
 	)
 }
---
+++ client/app/components/screens/auth/AuthFields.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react'
+import { Control } from 'react-hook-form'
+
+import Field from '@/components/ui/field/Field'
+
+import { IAuthFormData } from '@/types/auth.interface'
+
+import { validEmail } from './email.regex'
+
+interface IAuthFields {
+	control: Control<IAuthFormData>
+}
+
+const AuthFields: FC<IAuthFields> = ({ control }) => {
+	return (
+		<>
+			<Field<IAuthFormData>
+				placeholder='Введите email'
+				control={control}
+				name='email'
+				rules={{
+					required: 'Email обязателен',
+					pattern: {
+						value: validEmail,
+						message: 'Некорректный email'
+					}
+				}}
+				keyboardType='email-address'
+			/>
+			<Field<IAuthFormData>
+				placeholder='Введите пароль'
+				control={control}
+				name='password'
+				secureTextEntry
+				rules={{
+					required: 'Пароль обязателен',
+					minLength: {
+						value: 6,
+						message: 'Пароль должен состоять минимум из 6 символов'
+					}
+				}}
+			/>
+		</>
+	)
+}
+
+export default AuthFields
